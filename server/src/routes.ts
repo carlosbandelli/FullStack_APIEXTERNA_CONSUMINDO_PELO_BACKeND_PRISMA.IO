59cb94f614d43bb21701b2b3305754b1950da3b5
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2022-10-31 14:58:11 -0300

    Update routes.ts

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,10 +9,18 @@ const prisma = new PrismaClient({
 
 
 routes.get('/movies', async (req, res) => {
-  const movies = await prisma.film.findMany()
-  console.log(movies)
+  try {
 
-  return res.json(movies)
+  const movies = await prisma.film.findMany()
+  console.log('Parabens voce pegou todos os filmes')
+  console.log(movies)
+  console.log(`Quantidade de filmes do estudio: ${movies.length}`)
+
+  return res.status(200).json(movies)
+  }catch (error) {
+    console.log(`Mensagem de error: ${error}`)
+    return res.status(400).json({ message:error})
+  }
 })
   
 
