64b183975ebf368ba56c07600961402f76a08e06
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2022-11-01 16:04:20 -0300

    Delete server/prisma directory

--- server/prisma/migrations/20221027025613_create_table_film/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Film" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "director" TEXT NOT NULL,
-    "producer" TEXT NOT NULL,
-    "image" TEXT NOT NULL
-);
