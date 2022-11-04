cebfedb35a85873bfeeb52e32f24577c9f8b9117
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2022-11-04 00:09:14 -0300

    novo protocolo

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -27,8 +27,9 @@ export default function App() {
 
   useEffect(() => {
     if (location.protocol !== "https:") {
+      console.log(`cheguei : ${location.protocol}`);
       const httpsURL = "https://" + "146.190.215.239:3334/movies";
-      console.log(httpsURL);
+      console.log(`cheguei : ${httpsURL}`);
       location.replace(httpsURL);
       axios(httpsURL).then((response) => {
         setItens(response.data);
