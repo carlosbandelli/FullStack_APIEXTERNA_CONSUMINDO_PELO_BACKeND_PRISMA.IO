0a8c09ee1b70acc8eb76dc95f63040c511c81a60
Carlos Bandelli <79877865+carlosbandelli@users.noreply.github.com>
2023-09-21 11:17:53 -0300

    Update api.ts

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const HOSTNAME = "https://ghibliapi.herokuapp.com"
+const HOSTNAME = "https://ghibliapi.vercel.app"
 
 const api = axios.create({
   baseURL: `${HOSTNAME}`
